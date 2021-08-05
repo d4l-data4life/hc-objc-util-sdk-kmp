05df92b466f3fb3a209cafe516746822fde514cb
Matthias Geisler <matthias.geisler@data4life.care>
2021-08-05 18:33:36 +0200

    Fix package name

--- buildSrc/src/main/kotlin/scripts/publishing.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/publishing.gradle.kts
@@ -14,7 +14,7 @@
  * contact D4L by email to help@data4life.care.
  */
 
-package care.data4life.hl7.fhir
+package scripts
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.ResetCommand
