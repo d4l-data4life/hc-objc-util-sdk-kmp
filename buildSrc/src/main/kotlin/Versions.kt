71709b3b061df59d519d55fbfcc93ea40d95d8d3
Matthias Geisler <matthias.geisler@data4life.care>
2021-08-05 16:25:08 +0200

    Add gradle setup

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+object Versions {
+
+    object GradlePlugins {
+        const val kotlin = Versions.kotlin
+        const val android = "4.2.2"
+    }
+
+    // Kotlin
+    // https://github.com/JetBrains/kotlin
+    const val kotlin = "1.4.32"
+
+    // https://github.com/Kotlin/kotlinx.coroutines
+    const val kotlinCoroutines = "1.4.3-native-mt"
+
+    // https://github.com/touchlab/Stately
+    const val stately = "1.1.6"
+
+    // Network
+    // https://ktor.io/
+    const val ktor = "1.5.4"
+}
